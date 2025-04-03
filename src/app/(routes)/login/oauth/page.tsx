5fe87cfc778418f7390ad3d0903f13d5af08e1ec
nongdam <jungwook910318@gmail.com>
2025-04-04 04:41:09 +0900

    Update login/oauth/page.tsx
    JWT 디코딩 코드 추가.
    하지만 오류 있어서 주석 처리...

--- src/app/(routes)/login/oauth/page.tsx
+++ src/app/(routes)/login/oauth/page.tsx
@@ -4,7 +4,14 @@ import React, { Suspense, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import Header from "@/app/_components/Header";
+//import jwtDecode from "jwt-decode";
 import { fetchAccessTokenFromGoogle, LoginResponseProps } from "@/utils/apis/login";
+
+/*type Userinformation = {
+    sub: string;
+    new_user: boolean;
+    exp: number;
+};*/
 
 const GoogleCallback = () => {
     const searchParams = useSearchParams();
@@ -20,10 +27,17 @@ const GoogleCallback = () => {
 
                     if (data.access_token) {
                         if (typeof window === "undefined") return;
+
+                        //const decoded = jwtDecode<Userinformation>(data.access_token);
+
                         localStorage.setItem("access_token", data.access_token);
                         localStorage.setItem("refresh_token", data.refresh_token);
-                        localStorage.setItem("user_email", data.user_email);
+                        localStorage.setItem("user_email", data.user_email); //localStorage.setItem("user_email", decoded.sub);
+                        //localStorage.setItem("new_user", JSON.stringify(decoded.new_user));
+                        //localStorage.setItem("token_exp", decoded.exp.toString());
+
                         router.push("/");
+
                     } else {
                         console.error("Login Failed: No Access Token.");
                         alert("로그인에 실패했습니다.");
