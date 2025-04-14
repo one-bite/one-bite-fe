bac81616b269f2b8218ba76c7306962365365ec8
nongdam <jungwook910318@gmail.com>
2025-04-14 21:35:25 +0900

    feat: modify BigButton.tsx
    BigButton.tsx에 onClick() 추가
    문제 이어 풀기 버튼에 라우터 추가
    현재는 /quiz로 기본 라우팅 되게 함
    나중에 변경 필요

--- src/app/_components/base_components/BigButton.tsx
+++ src/app/_components/base_components/BigButton.tsx
@@ -4,16 +4,17 @@ import { Button } from "@nextui-org/react";
 interface BigButtonProps {
     children: React.ReactNode;
     className?: string;
+    onClick?: () => void;
 }
 
-const BigButton = ({ children, className = "" }: BigButtonProps) => {
+const BigButton = ({ children, className = "", onClick }: BigButtonProps) => {
     return (
-        <Button className={`bg-white rounded-3xl 
-        shadow-[0_12px_0] 
-        active:translate-y-[4px]
-        active:shadow-[0_8px_0] 
-        transition-all duration-150 ease-in-out
-        ${className}`}>
+        <Button
+            onClick={onClick}
+            className={`bg-white rounded-3xl shadow-[0_12px_0] 
+            active:translate-y-[4px] active:shadow-[0_8px_0] 
+            transition-all duration-150 ease-in-out
+            ${className}`}>
             {children}
         </Button>
     );
