81d63ff6197341bd72f1b2c39c29ae1cf7fe3c92
nongdam <jungwook910318@gmail.com>
2025-04-14 21:43:22 +0900

    Update ResumeCourseButton.tsx
    코스 진척도 UI 위치 표시
    버튼 가로 크기 300px로 늘림

--- src/app/_components/buttons/ResumeCourseButton.tsx
+++ src/app/_components/buttons/ResumeCourseButton.tsx
@@ -16,10 +16,13 @@ const ResumeCourseButton = ({courseName}: ResumeCourseButtonProps ) => {
     }
 
     return (
-        <BigButton onClick={handleClick} className="m-16 mt-4 p-8 w-[280px] h-[220px] bg-blue-400 hover:bg-blue-400 text-white justify-start items-start shadow-[0_12px_0_#3B82F6] active:shadow-[0_8px_0_#3B82F6]">
+        <BigButton onClick={handleClick} className="m-16 mt-4 p-8 w-[300px] h-[220px] bg-blue-400 hover:bg-blue-400 text-white justify-start items-start shadow-[0_12px_0_#3B82F6] active:shadow-[0_8px_0_#3B82F6]">
             <div className="text-left">
                 <h1 className="text-5xl font-jungM">{courseName}</h1>
                 <p className="text-lg font-linebold">문제 이어 풀기</p>
+                <div>
+                    여기에 코스 진척도 보여주는 UI
+                </div>
             </div>
         </BigButton>
     );
