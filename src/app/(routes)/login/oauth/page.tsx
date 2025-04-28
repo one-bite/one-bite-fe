46913099e11f5e645efe25c62ed7c07d1b9a9449
Seongwon_P <ikaros364125@kyonggi.ac.kr>
2025-04-28 20:21:00 +0900

    update(login): login in local
    oauth/page.tsx 수정
    UseRef 사용해 토큰 두 번 요청 방지
    로그인 오류 발생 시 push()가 아닌 replace()로 페이지 기록 덮어쓰기
    login.ts 수정
    오류 메시지 구별을 위해 숫자 추가

--- src/app/(routes)/login/oauth/page.tsx
+++ src/app/(routes)/login/oauth/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { Suspense, useEffect } from "react";
+import React, { Suspense, useEffect, useRef } from "react";
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
 import { jwtDecode } from "jwt-decode";
@@ -17,8 +17,12 @@ type Userinformation = {
 const GoogleCallback = () => {
     const searchParams = useSearchParams();
     const router = useRouter();
+    const isSentRef = useRef(false);
 
     useEffect(() => {
+        if (isSentRef.current) return;
+        isSentRef.current = true;
+
         const code = searchParams.get("code");
 
         if (code) {
@@ -40,18 +44,17 @@ const GoogleCallback = () => {
                         localStorage.setItem("token_exp", decoded.exp.toString());
 
 
-
-                        router.push("/");
+                        router.replace("/");
 
                     } else {
                         console.error("Login Failed: No Access Token.");
                         alert("로그인에 실패했습니다.");
-                        router.push("/login");
+                        router.replace("/login");
                     }
                 } catch (error) {
                     console.error("Login Failed: ", error);
                     alert("로그인 중 오류가 발생했습니다.\n잠시 후 다시 시도해주세요.");
-                    router.push("/login");
+                    router.replace("/login");
                 }
             };
 
