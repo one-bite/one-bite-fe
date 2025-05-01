ba47c188d3cdf38183f3d33b2d48d2ecd8920665
nongdam <jungwook910318@gmail.com>
2025-05-01 18:59:24 +0900

    Create log page.tsx
    유형 목록 구현 확인용 로그 페이지 생성

---
+++ src/app/(routes)/log/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+const Learn = () => {
+    return (
+        <div className="min-h-screen">
+            <div className="flex justify-center m-4 text-gray-500 text-xs">
+                <p>로그 페이지</p>
+            </div>
+        </div>
+    );
+};
+
+export default Learn;
