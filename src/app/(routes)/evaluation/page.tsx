0b3e3e156f0e80d6cbeaefbccf60012365754d0a
nongdam <jungwook910318@gmail.com>
2025-05-20 15:54:56 +0900

    Create page.tsx
    더미 영량평가 페이지 구현

---
+++ src/app/(routes)/evaluation/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+const Evaluation = () => {
+    return (
+        <div className="min-h-screen">
+            <div className="flex justify-center m-4 text-gray-500 text-xs">
+                <p>역량 평가용 페이지</p>
+            </div>
+        </div>
+    );
+};
+
+export default Evaluation;
