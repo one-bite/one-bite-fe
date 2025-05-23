678772c093365024d54c1bd4f15163fd1e4c2472
Seongwon_P <ikaros364125@kyonggi.ac.kr>
2025-05-23 11:34:46 +0900

    Update page.tsx

--- src/app/(routes)/challenge/page.tsx
+++ src/app/(routes)/challenge/page.tsx
@@ -165,7 +165,7 @@ const QuizPage = () => {
                     isCorrect={isCorrect}
                     correctAnswer={currentProblem.answer}
                     generatedByAI = {true}
-                    questionType={currentProblem.type}
+                    questionType={currentProblem.questionType}
                     lives={lives}
                     //topic={currentProblem.topic} //토픽도 주도록 api 수정 요청청
                 />
