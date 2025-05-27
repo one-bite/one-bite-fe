5c4f065a098dd874e53a18ca20535253e1e7a65b
nongdam <jungwook910318@gmail.com>
2025-05-27 22:30:20 +0900

    Update StartChallengeButton.tsx
    버튼 디자인 수정

--- src/app/_components/buttons/StartChallengeButton.tsx
+++ src/app/_components/buttons/StartChallengeButton.tsx
@@ -12,7 +12,7 @@ const StartChallengeButton = () => {
 
     return (
         <BigButton
-            className={""}
+            className={"flex items-center justify-center gap-2 w-64 h-12 text-white text-base font-line font-bold bg-lime-500 rounded-2xl shadow-[0_3px_0_#65A30D] /* lime-600 */ hover:bg-lime-400 active:translate-y-[3px] active:shadow-[0_-3px_0_#65A30D] transition-all duration-150 ease-in-out"}
             onClick={handleClick}
         >
             역량평가 하러가기
