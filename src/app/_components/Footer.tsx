2914602d9a784fe57195dcf2d310c172c4a5ce79
nongdam <jungwook910318@gmail.com>
2025-05-30 23:02:57 +0900

    Update Footer.tsx
    불필요한 footer 내용 제거

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -13,21 +13,6 @@ export default function Footer() {
             <span className="text-xs font-jungM">매일 10문제</span>
           </div>
           <div className="flex gap-4 justify-center">
-            {/* YouTube 아이콘 이미지 */}
-            <a
-              href="https://www.youtube.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-gray-600 hover:text-black"
-            >
-              <Image
-                src="/icons/youtube-icon.png"
-                alt="YouTube"
-                width={24}
-                height={24}
-                priority
-              />
-            </a>
             {/* GitHub 아이콘 이미지 */}
             <a
               href="https://github.com/one-bite"
@@ -44,35 +29,6 @@ export default function Footer() {
               />
             </a>
           </div>
-        </div>
-
-        {/* 중앙: Use cases */}
-        <div className="space-y-2">
-          <h4 className="text-xl font-semibold">Use cases</h4>
-          <ul className="space-y-1">
-            <li><a href="#" className="text-gray-600 hover:text-black">UI design</a></li>
-            <li><a href="#" className="text-gray-600 hover:text-black">UX design</a></li>
-            <li><a href="#" className="text-gray-600 hover:text-black">Wireframing</a></li>
-          </ul>
-        </div>
-
-        <div className="space-y-2">
-          <h4 className="text-xl font-semibold">Explore</h4>
-          <ul className="space-y-1">
-            <li><a href="#" className="text-gray-600 hover:text-black">Design</a></li>
-            <li><a href="#" className="text-gray-600 hover:text-black">Prototyping</a></li>
-            <li><a href="#" className="text-gray-600 hover:text-black">Development features</a></li>
-          </ul>
-        </div>
-
-        {/* 우측: Resources */}
-        <div className="space-y-2">
-          <h4 className="text-xl font-semibold">Resources</h4>
-          <ul className="space-y-1">
-            <li><a href="#" className="text-gray-600 hover:text-black">Blog</a></li>
-            <li><a href="#" className="text-gray-600 hover:text-black">Best practices</a></li>
-            <li><a href="#" className="text-gray-600 hover:text-black">Colors</a></li>
-          </ul>
         </div>
       </div>
     </footer>
