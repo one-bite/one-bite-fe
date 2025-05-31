c8aa699cd004903d96b1f330f01dc4d4e51ab6d8
nongdam <jungwook910318@gmail.com>
2025-05-31 19:39:43 +0900

    create CircleGraph
    원형 그래프 추가

---
+++ src/app/_components/graph/CircleGraph.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import {buildStyles, CircularProgressbar} from "react-circular-progressbar";
+
+interface CircleGraphProps {
+    value: number;
+    title?: string;
+    color: "blue" | "lime" | "orange" | "red" | "yellow" | "indigo" | "gray";
+    size: number;
+}
+
+const tailwindColorMap: Record<CircleGraphProps["color"], string> = {
+    blue: "#3b82f6",
+    lime: "#84cc16",
+    orange: "#f97316",
+    red: "#ef4444",
+    yellow: "#eab308",
+    indigo: "#6366f1",
+    gray: "#6b7280",
+};
+
+const CircleGraph  = ({value, title, color, size} : CircleGraphProps) => {
+    const hexColor = tailwindColorMap[color];
+
+    return (
+        <div className={`flex flex-col items-center justify-center`} style={{width:size, height:size}}>
+            <CircularProgressbar
+                value={value}
+                text={`${value}%`}
+                styles={buildStyles({
+                    pathColor:hexColor,
+                    textColor:"#1f2937",
+                    trailColor:"#e5e7eb",
+                    textSize:"20px",
+                })}
+            />
+            {!(!title) && (
+                <p className={"text-xs mt-1 text-center"} style={{color:hexColor}}>
+                    {title}
+                </p>
+            )}
+        </div>
+    )
+};
+
+export default CircleGraph;
