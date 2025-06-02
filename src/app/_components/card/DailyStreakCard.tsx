fba8bfed98e0045472059f9abde9468824d66674
nongdam <jungwook910318@gmail.com>
2025-06-03 02:24:48 +0900

    Update DailyStreakCard.tsx
    스트릭 카드 오늘의 문제 다 풀었을 때 출력 메세지 변경

--- src/app/_components/card/DailyStreakCard.tsx
+++ src/app/_components/card/DailyStreakCard.tsx
@@ -13,8 +13,8 @@ interface DailyStreakCardProps {
 
 const DailyStreakCard = ({ streakleftquiz, streakHistory }: DailyStreakCardProps) => {
     return (
-        <MainSectionCard className="w-full max-w-full md:max-w-[880px] m-2 md:m-8 p-0">
-            <div className="flex flex-col md:flex-row md:justify-between md:items-start w-full px-3 py-3 md:py-8 md:px-10">
+        <MainSectionCard className="w-full max-w-full md:max-w-[880px] m-2 md:my-8 md:mx-1 p-0">
+            <div className="flex flex-col md:flex-row md:justify-between md:items-start w-full px-3 py-2 md:py-4 md:px-10">
                 <div className="flex flex-col justify-start w-full md:w-1/2 gap-2 md:p-0 md:gap-3 overflow-hidden">
                     <div className="flex items-center gap-2 mb-1">
                         <div className={"bg-orange-500 rounded-full p-1"}>
@@ -23,12 +23,30 @@ const DailyStreakCard = ({ streakleftquiz, streakHistory }: DailyStreakCardProps
                         <span className={"text-lg font-linebold text-orange-500 md:text-2xl"}>데일리 스트릭</span>
                     </div>
                     <div className="flex flex-col gap-2 md:px-8 p-0 items-center md:items-start text-center md:text-left md:mt-4 mt-2">
-                        <h1 className="text-xl md:text-3xl font-linebold">
-                            오늘 스트릭 달성까지<br/>
-                            <span
-                                className="text-2xl md:text-4xl text-orange-500 font-linebold">{streakleftquiz}문제 </span>남았어요!
-                        </h1>
-                        <p className="text-sm md:text-sm text-gray-600 font-line mt-1">오늘의 문제를 풀고 연속 스트릭을 유지하세요</p>
+                        {streakleftquiz > 0 ? (
+                            <>
+                                <h1 className="text-xl md:text-3xl font-linebold">
+                                    오늘 스트릭 달성까지<br/>
+                                    <span
+                                        className="text-2xl md:text-4xl text-orange-500 font-linebold">{streakleftquiz}문제 </span>남았어요!
+                                </h1>
+                                <p className="text-sm md:text-sm text-gray-600 font-line mt-1">
+                                    오늘의 문제를 풀고 연속 스트릭을 유지하세요
+                                </p>
+                            </>
+                        ) : (
+                            <>
+                                <h1 className="text-xl md:text-3xl font-linebold">
+                                    오늘의 문제를<br/>
+                                    <span className="text-2xl md:text-4xl text-orange-500 font-linebold">
+                                        모두
+                                    </span> 풀었습니다!
+                                </h1>
+                                <p className="text-sm md:text-sm text-gray-600 font-line mt-1">
+                                    내일도 연속 스트릭을 이어가보세요!
+                                </p>
+                            </>
+                        )}
                     </div>
                 </div>
                 <div className="flex flex-col items-center justify-center w-full md:w-1/2 p-4 md:p-8 gap-2 md:gap-4">
