716ac1fcf3e5cb82ece2bb2f126539140e351be5
nongdam <jungwook910318@gmail.com>
2025-06-05 22:06:54 +0900

    Update Badge.tsx
    뱃지 컴포넌트

--- src/app/_configs/badge/Badge.tsx
+++ src/app/_configs/badge/Badge.tsx
@@ -2,24 +2,32 @@ import {badgeIconMap} from "app/_configs/badge/badgeIconMap";
 import {badgeColorMap} from "app/_configs/badge/badgeColorMap";
 
 interface BadgeProps {
-    name: string;
+    id: string;
     level: keyof typeof badgeColorMap;
     size?: number;
     labelColor?: string;
 }
 
-const Badge = ({name, level, size, labelColor="text-white"}:BadgeProps) => {
-    const Icon = badgeIconMap[name];
+const Badge = ({id, level, size = 64, labelColor="text-black"}:BadgeProps) => {
+    const icon = badgeIconMap[id];
+    if (!icon) return null;
+
+    const {name, icon: Icon} = icon;
     const bgColor = badgeColorMap[level] ?? "bg-gray-500";
 
+    const badgeSize = size * 0.6;
+
     return (
-        <div className={`${bgColor} inline-flex mx-2 md:mx-5 rounded-full pt-1 px-0.5 items-center justify-center mt-4 md:mt-5`}>
-            {Icon &&
-                <Icon className={'w-full h-full px-1 py-1'}/>
-            }
-            <span className={`${labelColor} font-linebold`}>
-                {name}
-            </span>
+        <div className="flex flex-col items-center mx-2 md:mx-3 mt-4 md:mt-5">
+            <div
+                className={`rounded-full flex items-center justify-center shadow ${bgColor}`}
+                style={{width: size, height: size}}
+            >
+                <Icon className={`text-white size-12`}/>
+            </div>
+            <span className={`mt-1 text-center ${labelColor} text-medium font-linebold`}>
+        {name}
+      </span>
         </div>
     );
 };
