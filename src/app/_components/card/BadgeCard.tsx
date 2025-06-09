e51e9ca85b92dc06c90ec6565723751a1134b0c8
nongdam <jungwook910318@gmail.com>
2025-06-09 22:52:39 +0900

    Update BadgeCard.tsx
    뱃지 카드 모바일에서는 제일 등급 높은 3개만

--- src/app/_components/card/BadgeCard.tsx
+++ src/app/_components/card/BadgeCard.tsx
@@ -8,6 +8,16 @@ import {userBadgeMap} from "app/_configs/badge/userBadgeMap";
 export default function BadgeCard() {
     const ownedBadgeIds = Object.keys(userBadgeMap);
 
+    const levelOrder = ["Diamond", "Platinum", "Gold", "Silver", "Bronze", "Iron"];
+
+    const sortedBadgeIds = ownedBadgeIds.sort((a, b) => {
+        const levelA = userBadgeMap[a].level;
+        const levelB = userBadgeMap[b].level;
+        return levelOrder.indexOf(levelA) - levelOrder.indexOf(levelB);
+    });
+
+    const topBadges = sortedBadgeIds.slice(0, 3);
+
     return (
         <MainSectionCard minHeight="240px" className={'md:col-span-2'}>
             <div className="flex flex-col justify-betweenl w-full h-full gap-4">
@@ -18,7 +28,20 @@ export default function BadgeCard() {
                     <h2 className="text-xl font-linebold text-pink-600">획득한 뱃지</h2>
                 </div>
                 <div
-                    className="flex flex-wrap gap-2 items-center justify-center w-full md:w-full h-full bg-gray-100 rounded-lg overflow-x-auto md:max-w-full">
+                    className="md:hidden flex flex-wrap gap-2 items-center justify-center w-full md:w-full h-full bg-gray-100 rounded-lg overflow-x-auto md:max-w-full">
+                    {topBadges.map((id) => {
+                        const {level} = userBadgeMap[id];
+                        return (
+                            <Badge
+                                key={id}
+                                id={id}
+                                level={level}
+                            />
+                        )
+                    })}
+                </div>
+                <div
+                    className="hidden md:flex flex-wrap gap-2 items-center justify-center w-full md:w-full h-full bg-gray-100 rounded-lg overflow-x-auto md:max-w-full">
                     {ownedBadgeIds.map((id) => {
                         const {level} = userBadgeMap[id];
                         return (
