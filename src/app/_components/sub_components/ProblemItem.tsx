26003eb4340e054f6bab475b124dbc8856f54112
nongdam <jungwook910318@gmail.com>
2025-06-10 14:32:18 +0900

    Update ProblemItem.tsx
    문제 목록 아이템 배경 선택 색과 문제 제목 패딩 수정

--- src/app/_components/sub_components/ProblemItem.tsx
+++ src/app/_components/sub_components/ProblemItem.tsx
@@ -14,9 +14,9 @@ const ProblemItem = ({ id, title, choose, isCorrect }: ProblemItemProps) => {
     const titleColor = isCorrect == null ? "text-black" : isCorrect ? "text-green-900" : "text-red-700 bg-red-50 rounded-lg";
 
     return (
-        <div className={`flex px-1 py-1 hover:bg-gray-300 active:bg-gray-400 selection:bg-gray-400 md:hover:bg-gray-300 md:active:bg-gray-400 md:selection:bg-gray-400 rounded-lg cursor-pointer`} onClick={choose}>
+        <div className={`flex px-1 py-1 hover:bg-gray-300 active:bg-gray-200 selection:bg-gray-200 md:hover:bg-gray-300 md:active:bg-gray-200 md:selection:bg-gray-200 rounded-lg cursor-pointer`} onClick={choose}>
             <span className="font-line text-medium text-gray-500 mr-2 shrink-0">{id}. </span>
-            <span className={`w-full text-medium font-line truncate ${titleColor}`}>{title}</span>
+            <span className={`w-full px-1 text-medium font-line truncate ${titleColor}`}>{title}</span>
             <span>
                 {isCorrect === true && <CheckIcon className={"w-5 h-5 text-green-500"}/>}
                 {isCorrect === false && <XMarkIcon className={'w-5 h-5 text-red-500'}/>}
