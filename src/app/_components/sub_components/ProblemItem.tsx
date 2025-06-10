f73090a4daca89cd8c4aa265d9810dcc861e8271
nongdam <jungwook910318@gmail.com>
2025-06-10 13:39:23 +0900

    Update ProblemItem.tsx
    로그 문제 정/오답 디자인 수정

--- src/app/_components/sub_components/ProblemItem.tsx
+++ src/app/_components/sub_components/ProblemItem.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React from "react";
+import { CheckIcon, XMarkIcon } from "@heroicons/react/24/solid";
 
 interface ProblemItemProps {
     id: number | string;
@@ -10,12 +11,16 @@ interface ProblemItemProps {
 }
 
 const ProblemItem = ({ id, title, choose, isCorrect }: ProblemItemProps) => {
-    const titleColor = isCorrect == null ? "text-black" : isCorrect ? "text-green-500" : "text-orange-500";
+    const titleColor = isCorrect == null ? "text-black" : isCorrect ? "text-green-900 bg-green-200" : "text-red-700 font-linebold";
 
     return (
         <div className={`flex px-1 py-1 hover:bg-gray-300 active:bg-gray-400 selection:bg-gray-400 md:hover:bg-gray-300 md:active:bg-gray-400 md:selection:bg-gray-400 rounded-lg cursor-pointer`} onClick={choose}>
             <span className="font-line text-medium text-gray-500 mr-2 shrink-0">{id}. </span>
             <span className={`w-full text-medium font-line truncate ${titleColor}`}>{title}</span>
+            <span>
+                {isCorrect === true && <CheckIcon className={"w-5 h-5 text-green-500"}/>}
+                {isCorrect === false && <XMarkIcon className={'w-5 h-5 text-red-500'}/>}
+            </span>
         </div>
     );
 };
